886808e204ead5ee05aba29cd6801a1af9a950af
crackcodesprogramloads <mikepadial@gmail.com>
2023-08-16 18:09:58 +0800

    add title bar and sign in prompt

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,18 +1,13 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import { Flex } from '@mantine/core';
 
 import SearchShortcut from './components/SearchShortcut/Container';
 import SoftwareList from './components/SoftwareList/Container';
 import ShortcutList from './components/ShortcutList/Container';
-import BrandLogo from './components/common/BrandLogo';
 import LayoutProvider from './layout/LayoutProvider';
-import useUser from './hooks/useUser';
-import SignInPrompt from './components/Auth/SignInPrompt';
 import Layout from './layout/Layout';
 import useFetchSoftwareShortcuts from './hooks/useFetchSoftwareShortcuts';
 
 function Main() {
-  const user = useUser();
   useFetchSoftwareShortcuts();
 
   return (
@@ -21,10 +16,6 @@ function Main() {
         <SearchShortcut />
         <SoftwareList />
         <ShortcutList />
-        <Flex>
-          <SignInPrompt isDisplayed={!user} />
-          <BrandLogo />
-        </Flex>
       </Layout>
     </LayoutProvider>
   );
--- src/renderer/components/TitleBar.tsx
+++ src/renderer/components/TitleBar.tsx
@@ -1,17 +1,20 @@
-// import { BrowserWindow } from 'electron';
+import { ipcRenderer } from 'electron';
 import mainWindow from 'main/mainWindow';
-
-import CLOSE from '../../../assets/close.svg';
-import MINIMIZE from '../../../assets/minimize.svg';
-import MAXIMIZE from '../../../assets/maximize.svg';
 
 function TitleBar() {
   // const renderer = ipcRenderer.invoke;
   const window = mainWindow.getWindow();
+  // window.ipcRender.send('window:minimize');
+
+  // window.ipcRender.send('window:restore');
+
+  // window.ipcRender.send('window:maximize');
+
+  // window.ipcRender.send('window:close');
 
   function CloseWindow() {
     console.log('close');
-    const close = window?.close();
+    const close = window?.ipcRenderer.send('window:close');
     return close;
     // return window!.on('closed', () => {
     //   mainWindow.setWindow(null);
@@ -39,31 +42,68 @@ function TitleBar() {
     <div className="titlebar bg-cyan-950 py-3 px-5 h-[20px] flex flex-row items-center justify-center">
       <div className="h-full w-[54px] flex items-center">
         <button
-          className="titlebutton invert cursor-pointer"
+          className="hover:text-blue-500 cursor-pointer"
           type="button"
           onClick={CloseWindow}
         >
-          <img src={CLOSE} width="14px" height="14px" alt="close" />
+          <svg
+            className="titlebutton"
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 16 16"
+            height="14px"
+            width="14px"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"
+            />
+          </svg>
         </button>
         <button
-          className="titlebutton invert cursor-pointer"
+          className="hover:text-blue-500 cursor-pointer"
           type="button"
           onClick={MinimizeWindow}
         >
-          <img src={MINIMIZE} width="20px" height="20px" alt="minimize" />
+          <svg
+            className="titlebutton"
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 16 16"
+            height="20px"
+            width="20px"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M14 8v1H3V8h11z" />
+          </svg>
         </button>
         <button
-          className="titlebutton invert cursor-pointer"
+          className="hover:text-blue-500 cursor-pointer"
           type="button"
           onClick={MaximizeWindow}
         >
-          <img src={MAXIMIZE} width="16px" height="16px" alt="maximize" />
+          <svg
+            className="titlebutton"
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 16 16"
+            height="16px"
+            width="16px"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M3 3v10h10V3H3zm9 9H4V4h8v8z" />
+          </svg>
         </button>
       </div>
       <button
         id="button"
         type="button"
-        className="titlebutton w-full h-full flex items-center justify-end cursor-pointer hover:text-blue-500"
+        className="w-full h-full flex items-center justify-end cursor-pointer hover:text-blue-500"
       >
         <p className="titlebutton">Shortcut Wizard</p>
       </button>
